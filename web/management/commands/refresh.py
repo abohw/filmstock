24ec9d353fae0aec3f316ea460d5c1d0b67b3c4d
Alex Bowman <alex@abxmb.com>
2020-12-31 10:54:12 -0600

    lets get this party started

--- web/management/commands/refresh.py
+++ web/management/commands/refresh.py
@@ -1,8 +1,8 @@
 from django.core.management import BaseCommand
 from django.conf import settings
 from web.models import Camera
-
-from datetime import datetime, timedelta
+from django.utils import timezone
+import pytz
 
 class Command(BaseCommand):
     help = 'what'
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
         x = 0
 
-        for camera in Camera.objects.filter(lastSeen__lt=(datetime.now() - timedelta(hours=4))):
+        for camera in Camera.objects.filter(lastSeen__lt=(timezone.now() - timezone.timedelta(hours=4))):
             print('deleting %s from %s' % (camera.name, camera.source))
             camera.delete()
             x += 1
