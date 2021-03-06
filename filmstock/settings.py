ff56fbebc18b49cbd2b0ad842abcd1d742104770
Alex Bowman <alex@abxmb.com>
2021-03-06 17:25:51 -0600

    style

--- filmstock/settings.py
+++ filmstock/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
